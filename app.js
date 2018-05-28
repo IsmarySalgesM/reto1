ca96c05d7394ed426fea32c0044e5d34f773c67f
Ismary Salges <ismarysalges@gmail.com>
2018-05-28 17:55:21 -0400

    Dos funciones suma y multiplicar

---
+++ app.js
@@ -0,0 +1,29 @@
+function add (suma1,suma2) {
+       return suma1 + suma2; 
+}            
+function multiply(multi1,multi2) {
+        return multi1 * multi2;
+}           
+const resulsuma = add (9824,777); 
+const total = multiply (36325,resulsuma);
+document.write(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+    
